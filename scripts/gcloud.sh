6b0b890bbb00a924a34f8f55715443c30dc35de9
MisterLobo <algae1234567890@gmail.com>
2025-05-03 05:01:38 +0800

    moved version check

--- scripts/gcloud.sh
+++ scripts/gcloud.sh
@@ -16,6 +16,7 @@ curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-
 ls
 tar -xf "$CWD/google-cloud-cli-linux-x86_64.tar.gz"
 
+"$CWD/google-cloud-sdk/bin/gcloud" --version
 "$CWD/google-cloud-sdk/bin/gcloud" $@
 
 #run deploy $_SERVICE_NAME --image '$_AR_HOSTNAME/$PROJECT_ID/$_AR_REPO/$_SERVICE_NAME:$COMMIT_SHA' --region $_DEPLOY_REGION --platform $_PLATFORM
