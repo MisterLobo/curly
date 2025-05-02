efca0a39889ac6154a8bfb6e6b25c6d3e6502e65
MisterLobo <algae1234567890@gmail.com>
2025-05-03 04:53:40 +0800

    use non interactive install

--- scripts/gcloud.sh
+++ scripts/gcloud.sh
@@ -7,7 +7,7 @@ ARGS=$@
 echo $ARGS
 
 apt-get update
-apt-get install curl tar
+apt-get install -y curl tar
 
 CWD=$(pwd)
 
@@ -16,6 +16,6 @@ curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-
 ls
 tar -xf "$CWD/google-cloud-cli-linux-x86_64.tar.gz"
 
-./google-cloud-sdk/bin/gcloud $@
+"$CWD/google-cloud-sdk/bin/gcloud" $@
 
 #run deploy $_SERVICE_NAME --image '$_AR_HOSTNAME/$PROJECT_ID/$_AR_REPO/$_SERVICE_NAME:$COMMIT_SHA' --region $_DEPLOY_REGION --platform $_PLATFORM
