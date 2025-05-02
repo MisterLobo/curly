607f8631195b2fc96800ed56c9aab770626257cd
MisterLobo <algae1234567890@gmail.com>
2025-05-03 05:22:25 +0800

    pass env vars

--- scripts/gcloud.sh
+++ scripts/gcloud.sh
@@ -6,6 +6,15 @@ ARGS=$@
 
 echo $ARGS
 
+echo $SERVICE_NAME
+echo $AR_HOSTNAME
+echo $PROJECT_ID
+echo $AR_REPO
+echo $SERVICE_NAME
+echo $COMMIT_SHA
+echo $DEPLOY_REGION
+echo $PLATFORM
+
 apt-get update
 apt-get install -y curl tar
 
@@ -17,6 +26,9 @@ ls
 tar -xf "$CWD/google-cloud-cli-linux-x86_64.tar.gz"
 
 "$CWD/google-cloud-sdk/bin/gcloud" --version
-"$CWD/google-cloud-sdk/bin/gcloud" $@
+"$CWD/google-cloud-sdk/bin/gcloud" run deploy $SERVICE_NAME \
+  --image "$AR_HOSTNAME/$PROJECT_ID/$AR_REPO/$SERVICE_NAME:$COMMIT_SHA" \
+  --region $DEPLOY_REGION \
+  --platform $PLATFORM
 
 #run deploy $_SERVICE_NAME --image '$_AR_HOSTNAME/$PROJECT_ID/$_AR_REPO/$_SERVICE_NAME:$COMMIT_SHA' --region $_DEPLOY_REGION --platform $_PLATFORM
